0b2c1347039d4bc2979d8e42c4016ebbf802c5c7
eljayman <lelandoj@gmail.com>
2022-12-19 21:10:28 -0700

    added Pet class

---
+++ models/Pet.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Pet extends Model {}
+
+Pet.init(
+  {
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = Pet;
