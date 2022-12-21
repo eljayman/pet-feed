c2a902c0e333ba065c27931966e38278201c7051
eljayman <lelandoj@gmail.com>
2022-12-20 20:28:41 -0700

    added seeds for users and updated related files

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const routes = require("./routes");
-const sequelize = require("./config/connection");
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));.
+app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
