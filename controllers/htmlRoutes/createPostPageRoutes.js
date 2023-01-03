b3f6a8f12f3251f8a4be17de4e47bddc9e5e53e9
Rhondoid <110504360+Rhondoid@users.noreply.github.com>
2023-01-02 21:54:21 -0700

    added create post handlebars and routes file

---
+++ controllers/htmlRoutes/createPostPageRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { Post, User } = require('../../models');
+const withAuth = require('../../utils/auth')
+
+router.get('/', withAuth, async (req, res) => {
+  
+
+    res.render('createPostPage')
+  
+});
+
+module.exports = router;
