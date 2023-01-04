bb7a67b11c8a594348cf5d1ef0dc89b3c3e8e9cb
eljayman <112910305+eljayman@users.noreply.github.com>
2023-01-03 21:47:07 -0700

    Update controllers/api/user-routes.js
    Co-authored-by: Jace Edwards <100460302+Jacee94@users.noreply.github.com>

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -19,7 +19,7 @@ router.post('/createUser', async (req, res) => {
     const userData = await User.create(req.body);
     //log user in after it is created
     req.session.save(() => {
-      req.session.user_id = newUser.id;
+      req.session.user_id = userData.id;
       req.session.logged_in = true;
       res.status(200).json(userData);
     });
