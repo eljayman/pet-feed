2ecbf445b5b1838684e16c9dc8aa02645defb46a
Brent Keller <bikeller104@gmail.com>
2023-01-05 19:26:05 -0700

    added with auth middleware to profile page

--- controllers/htmlRoutes/profilePageRoutes.js
+++ controllers/htmlRoutes/profilePageRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Post, User } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
   try {
     const userData = await User.findOne({
       where: { id: req.session.user_id },
